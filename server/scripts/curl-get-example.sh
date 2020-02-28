47188c871e5e908b47b237430d519157c9ac8dfe
dinies <edoardo.ghini@live.it>
2020-02-28 21:06:54 +0100

    first attempt to set up a simple webserver that calls a python script when receives a GET request

---
+++ server/scripts/curl-get-example.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env node
+
+curl http://localhost:3000/endpoint-name?arg1=1&arg2=20&arg3=300&arg4=4000
---
+++ src/convert.py
@@ -0,0 +1,9 @@
+import sys
+
+arg1 = argv[1]
+arg2 = argv[2]
+arg3 = argv[3]
+arg4 = argv[4]
+
+
+return arg1 + arg2+ arg3 + arg4
